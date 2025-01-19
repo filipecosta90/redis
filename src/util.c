a8c89eed44899b12b2f72b039553edd948f4b6c1
fcostaoliveira <filipe@redis.com>
2025-01-19 19:34:25 +0000

    using lemire's/hackers Hacker’s Delight 64-bit integers digit count

--- src/util.c
+++ src/util.c
@@ -351,26 +351,33 @@ char *memmapchars(char *s, size_t len, const char *from, const char *to, size_t 
     return s;
 }
 
+int int_log2(uint64_t x) { return 63 - __builtin_clzll(x | 1); }
+
 /* Return the number of digits of 'v' when converted to string in radix 10.
  * See ll2string() for more information. */
 uint32_t digits10(uint64_t v) {
-    if (v < 10) return 1;
-    if (v < 100) return 2;
-    if (v < 1000) return 3;
-    if (v < 1000000000000UL) {
-        if (v < 100000000UL) {
-            if (v < 1000000) {
-                if (v < 10000) return 4;
-                return 5 + (v >= 100000);
-            }
-            return 7 + (v >= 10000000UL);
-        }
-        if (v < 10000000000UL) {
-            return 9 + (v >= 1000000000UL);
-        }
-        return 11 + (v >= 100000000000UL);
-    }
-    return 12 + digits10(v / 1000000000000UL);
+    static uint64_t table[] = {9,
+                                99,
+                                999,
+                                9999,
+                                99999,
+                                999999,
+                                9999999,
+                                99999999,
+                                999999999,
+                                9999999999,
+                                99999999999,
+                                999999999999,
+                                9999999999999,
+                                99999999999999,
+                                999999999999999ULL,
+                                9999999999999999ULL,
+                                99999999999999999ULL,
+                                999999999999999999ULL,
+                                9999999999999999999ULL};
+    int y = (19 * int_log2(v) >> 6);
+    y += v > table[y];
+    return y + 1;
 }
 
 /* Like digits10() but for signed values. */
