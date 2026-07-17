896163f2c6e8c13ef4d3e2746aab3755b10809a7
fcostaoliveira <filipe@redis.com>
2026-07-17 16:06:20 +0100

    bench: distinct-hash datapoint 1 (comment, binary-identical)

--- src/t_stream.c
+++ src/t_stream.c
@@ -6320,3 +6320,5 @@ static void idmpEvictOldestEntry(stream *s, idmpProducer *producer) {
     dictDelete(producer->idmp_dict, oldest);
     idmpEntryFree(oldest, &s->alloc_size);
 }
+
+/* bench distinct-hash datapoint 1 (no-op comment) */
